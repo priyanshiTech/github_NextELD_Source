ddd9b53129efe53f28ca0af33bdb334cc157afe8
Priyanshi Tech <priyanshi.inurum@gmail.com>
2026-01-16 18:46:57 +0530

    web

--- NextEld/View/HomeView /DailyLogs/Webview.swift
+++ NextEld/View/HomeView /DailyLogs/Webview.swift
@@ -1,63 +1,119 @@
+////
+////  Webview.swift
+////  NextEld
+////
+////  Created by priyanshi  on 19/08/25.
+////
 //
-//  Webview.swift
-//  NextEld
-//
-//  Created by priyanshi  on 19/08/25.
-//
-
 import Foundation
+//import SwiftUI
+//import WebKit
+//
+//struct WebView: UIViewRepresentable {
+//    let url: URL
+//    @Binding var isLoading: Bool
+//    
+//    func makeUIView(context: Context) -> WKWebView {
+//        let webView = WKWebView()
+//        webView.navigationDelegate = context.coordinator
+//        return webView
+//    }
+//    
+//    func updateUIView(_ webView: WKWebView, context: Context) {
+//        if webView.url != url {
+//            webView.load(URLRequest(url: url))
+//        }
+//    }
+//    
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(isLoading: $isLoading)
+//    }
+//    
+//    final class Coordinator: NSObject, WKNavigationDelegate {
+//        @Binding var isLoading: Bool
+//        
+//        init(isLoading: Binding<Bool>) {
+//            _isLoading = isLoading
+//        }
+//        
+//        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+//            DispatchQueue.main.async {
+//                self.isLoading = true
+//            }
+//        }
+//        
+//        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//            DispatchQueue.main.async {
+//                self.isLoading = false
+//            }
+//        }
+//        
+//        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+//            DispatchQueue.main.async {
+//                self.isLoading = false
+//            }
+//        }
+//        
+//        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+//            DispatchQueue.main.async {
+//                self.isLoading = false
+//            }
+//        }
+//    }
+//}
+
+
 import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
+
     let url: URL
     @Binding var isLoading: Bool
-    
+
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+
+        let config = WKWebViewConfiguration()
+        config.preferences.javaScriptEnabled = true
+        config.defaultWebpagePreferences.allowsContentJavaScript = true
+
+        let webView = WKWebView(frame: .zero, configuration: config)
         webView.navigationDelegate = context.coordinator
+        webView.scrollView.isScrollEnabled = true
+        webView.isOpaque = false
+        webView.backgroundColor = .white
+
         return webView
     }
-    
+
     func updateUIView(_ webView: WKWebView, context: Context) {
-        if webView.url != url {
-            webView.load(URLRequest(url: url))
-        }
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData)
+        webView.load(request)
     }
-    
+
     func makeCoordinator() -> Coordinator {
         Coordinator(isLoading: $isLoading)
     }
-    
+
     final class Coordinator: NSObject, WKNavigationDelegate {
+
         @Binding var isLoading: Bool
-        
+
         init(isLoading: Binding<Bool>) {
             _isLoading = isLoading
         }
-        
+
         func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-            DispatchQueue.main.async {
-                self.isLoading = true
-            }
+            DispatchQueue.main.async { self.isLoading = true }
         }
-        
+
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            DispatchQueue.main.async {
-                self.isLoading = false
-            }
+            DispatchQueue.main.async { self.isLoading = false }
         }
-        
-        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
-            DispatchQueue.main.async {
-                self.isLoading = false
-            }
-        }
-        
+
         func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
-            DispatchQueue.main.async {
-                self.isLoading = false
-            }
+            DispatchQueue.main.async { self.isLoading = false }
+            print("❌ WebView load failed:", error.localizedDescription)
         }
     }
 }
